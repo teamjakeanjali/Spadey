5432f8a317670fef1e72aec410c47cadb3045d42
Anjali Ahuja <anjaliannahuja@gmail.com>
2018-05-09 17:38:30 -0500

    working emotion analysis response from post

--- helpers/voiceAnalysis.js
+++ helpers/voiceAnalysis.js
@@ -5,14 +5,19 @@ const ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
 // var SoxCommand = require('sox-audio');
 // var command = SoxCommand();
 
-router.post('/audio', function(req, res) {
-  // convertAudio('./assets/test.wav');
+// sets up tone analyzer credentials
+let toneAnalyzer = new ToneAnalyzerV3({
+  username: '1813adc9-1239-402c-9adb-7c5bfdb95664',
+  password: 'pOt86kbRyD6A',
+  version: '2017-09-21'
+});
 
+router.post('/audio', function(req, res) {
   // Creates a client
   const client = new speech.SpeechClient();
 
   // configuration needed for sync speech recognize
-  const filename = './assets/test1.flac';
+  const filename = './assets/test.flac';
   const encoding = 'FLAC';
   const sampleRateHertz = 44100;
   const languageCode = 'en-US';
@@ -37,38 +42,33 @@ router.post('/audio', function(req, res) {
     .recognize(request)
     .then(data => {
       const response = data[0];
-      const transcription = response.results
+      return response.results
         .map(result => result.alternatives[0].transcript)
         .join('\n');
-      console.log('Transcription: ', transcription);
-      res.send({
-        transcription: transcription,
-        analysis: analyzeText(transcription)
+    })
+    .then(transcription => {
+      //Set up tone analyzer params
+      let params = {
+        tone_input: { text: transcription },
+        content_type: 'application/json'
+      };
+
+      //Do tone analysis
+      toneAnalyzer.tone(params, function(error, response) {
+        if (error) {
+          console.log('error:', error);
+        } else {
+          res.status(201).send({
+            transcription: transcription,
+            analysis: JSON.stringify(response)
+          });
+        }
       });
     })
     .catch(err => {
       console.error('ERROR:', err);
     });
 });
-// sets up tone analyzer credentials
-let toneAnalyzer = new ToneAnalyzerV3({
-  username: '1813adc9-1239-402c-9adb-7c5bfdb95664',
-  password: 'pOt86kbRyD6A',
-  version: '2017-09-21'
-});
-
-//API call to analyze sentiment
-const analyzeText = text => {
-  let params = {
-    tone_input: { text: text },
-    content_type: 'application/json'
-  };
-
-  toneAnalyzer.tone(params, function(error, response) {
-    if (error) console.log('error:', error);
-    else console.log(JSON.stringify(response, null, 2));
-  });
-};
 
 //CONVERT TO FLAC FILE
 // const convertAudio = inputStream => {
