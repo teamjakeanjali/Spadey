c5c79768c417787d76aea4db2bc0660535d8e971
Anjali Ahuja <anjaliannahuja@gmail.com>
2018-05-15 18:57:47 -0500

    working link coming back from file conversion api

--- helpers/voiceAnalysis.js
+++ helpers/voiceAnalysis.js
@@ -1,16 +1,6 @@
-const express = require('express');
-const router = require('express').Router();
 const fs = require('fs');
 const speech = require('@google-cloud/speech').v1p1beta1;
 const ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
-const bodyParser = require('body-parser');
-const FileReader = require('filereader');
-let reader = new FileReader();
-
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-
-//Convert webm to flac
 
 // sets up tone analyzer credentials
 let toneAnalyzer = new ToneAnalyzerV3({
@@ -19,72 +9,59 @@ let toneAnalyzer = new ToneAnalyzerV3({
   version: '2017-09-21'
 });
 
-var src = fs.createReadStream('./assets/audio.webm');
-var transcode = sox({
-  output: {
-    bits: 16,
-    rate: 44100,
-    channels: 1,
-    type: 'flac'
-  }
-});
-var dest = fs.createWriteStream('./assets/audio.flac');
-src.pipe(transcode).pipe(dest);
+const speechAnalysis = () => {
+  // Creates a client
+  const client = new speech.SpeechClient();
+  // configuration needed for sync speech recognize
+  const filename = './assets/output.flac';
+  const encoding = 'FLAC';
+  const sampleRateHertz = 44100;
+  const languageCode = 'en-US';
+  const config = {
+    encoding: encoding,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode,
+    enableAutomaticPunctuation: true
+  };
+  const audio = {
+    content: fs.readFileSync(filename).toString('base64')
+  };
+  const speechRequest = {
+    config: config,
+    audio: audio
+  };
+  // Detects speech in the audio file
+  client
+    .recognize(speechRequest)
+    .then(data => {
+      const response = data[0];
+      const transcription = response.results
+        .map(result => result.alternatives[0].transcript)
+        .join('\n');
+      console.log('TRANSCRIPTION', transcription);
+    })
+    .then(transcription => {
+      //Set up tone analyzer params
+      let params = {
+        tone_input: { text: transcription },
+        content_type: 'application/json'
+      };
+      // Do tone analysis
+      //   toneAnalyzer.tone(params, function(error, response) {
+      //     if (error) {
+      //       console.log('error:', error);
+      //     } else {
+      //       // res.status(201).send({
+      //       //   transcription: transcription,
+      //       //   analysis: JSON.stringify(response)
+      //       // });
+      //     }
+      //   });
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
+};
 
-transcode.on('error', function(err) {
-  console.log('oh no! ' + err.message);
-});
-
-//Creates a client
-// const client = new speech.SpeechClient();
-// // configuration needed for sync speech recognize
-// // const filename = './assets/audio.flac';
-// const encoding = 'FLAC';
-// const sampleRateHertz = 44100;
-// const languageCode = 'en-US';
-// const config = {
-//   encoding: encoding,
-//   sampleRateHertz: sampleRateHertz,
-//   languageCode: languageCode,
-//   enableAutomaticPunctuation: true
-// };
-// const audio = {
-//   content: fs.readFileSync(filename).toString('base64'),
-// };
-// const request = {
-//   config: config,
-//   audio: audio
-// };
-// // Detects speech in the audio file
-// client
-//   .recognize(request)
-//   .then(data => {
-//     const response = data[0];
-//     const transcription = response.results
-//       .map(result => result.alternatives[0].transcript)
-//       .join('\n');
-//     console.log(transcription);
-//   })
-//   .then(transcription => {
-//     //Set up tone analyzer params
-//     let params = {
-//       tone_input: { text: transcription },
-//       content_type: 'application/json'
-//     };
-//     //Do tone analysis
-//     // toneAnalyzer.tone(params, function(error, response) {
-//     //   if (error) {
-//     //     console.log('error:', error);
-//     //   } else {
-//     //     res.status(201).send({
-//     //       transcription: transcription,
-//     //       analysis: JSON.stringify(response)
-//     //     });
-//     //   }
-//     // });
-//   })
-//   .catch(err => {
-//     console.error('ERROR:', err);
-//   });
-
-module.exports = router;
+module.exports = convertToFlac;
+module.exports = speechAnalysis;
--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const express = require('express');
 const app = express();
 const isDevMode = process.env.NODE_ENV === 'development';
 const request = require('request');
-const voiceRoutes = require('./helpers/voiceAnalysis.js');
+const voiceAnalysis = require('./helpers/voiceAnalysis.js');
 const socket = require('socket.io');
 const ss = require('socket.io-stream');
 const fs = require('fs');
+
+const speech = require('@google-cloud/speech').v1p1beta1;
 
 app.use(require('morgan')('short'));
 
@@ -33,7 +35,7 @@ app.use(require('morgan')('short'));
   app.use(express.static(__dirname + '/'));
 })();
 
-app.use('/message', voiceRoutes);
+// app.use('/message', voiceRoutes);
 
 app.get(/.*/, function root(req, res) {
   res.sendFile(__dirname + '/src/index.html');
@@ -50,8 +52,132 @@ server.listen(process.env.PORT || 3000, function onListen() {
 const io = socket(server);
 
 io.of('/audio').on('connection', function(socket) {
-  ss(socket).on('send-audio', function(stream, data) {
+  ss(socket).on('send-audio', async (stream, data) => {
     const fileName = 'assets/audio.webm';
-    stream.pipe(fs.createWriteStream(fileName));
+    await stream.pipe(fs.createWriteStream(fileName));
   });
 });
+
+let fileId = '';
+const uploadWebmFile = () => {
+  const dataString = {
+    apikey: '53083a86c10baf8ceee6e2054873c806',
+    input: 'upload',
+    filename: './assets/audio.webm',
+    outputformat: 'flac'
+  };
+
+  var options = {
+    url: 'http://api.convertio.co/convert',
+    method: 'POST',
+    body: dataString,
+    json: true
+  };
+
+  function callback(error, response) {
+    console.log(response);
+    if (!error && response.statusCode == 200) {
+      let body = response.body;
+      fileId = body.data.id;
+      console.log(fileId);
+    }
+  }
+
+  request(options, callback);
+};
+
+const directUpload = () => {
+  const file = {
+    file: './assets/audio.webm'
+  };
+
+  var options = {
+    url:
+      'http://api.convertio.co/convert/ec24567c3a62b0396b16e54ab5847f1d/audio.webm',
+    method: 'PUT'
+  };
+
+  function callback(error, response) {
+    if (!error && response.statusCode == 200) {
+      console.log(response);
+    }
+  }
+
+  fs.createReadStream('./assets/audio.webm').pipe(request(options, callback));
+};
+
+const getFlacFile = () => {
+  var options = {
+    url:
+      'http://api.convertio.co/convert/ec24567c3a62b0396b16e54ab5847f1d/status'
+  };
+
+  function callback(error, response) {
+    console.log(error);
+    console.log(response);
+    if (!error && response.statusCode == 200) {
+      console.log(response.data);
+    }
+  }
+
+  request(options, callback);
+};
+
+// uploadWebmFile();
+// directUpload();
+getFlacFile();
+
+// const client = new speech.SpeechClient();
+// // configuration needed for sync speech recognize
+// const encoding = 'FLAC';
+// const sampleRateHertz = 48000;
+// const filename = './assets/output.flac';
+// const languageCode = 'en-US';
+
+// const config = {
+//   encoding: encoding,
+//   sampleRateHertz: sampleRateHertz,
+//   languageCode: languageCode,
+//   enableAutomaticPunctuation: true
+// };
+
+// const audio = {
+//   content: fs.readFileSync(filename).toString('base64')
+// };
+
+// const speechRequest = {
+//   config: config,
+//   audio: audio
+// };
+
+// // Detects speech in the audio file
+// client
+//   .recognize(speechRequest)
+//   .then(data => {
+//     const response = data[0];
+//     const transcription = response.results
+//       .map(result => result.alternatives[0].transcript)
+//       .join('\n');
+//     console.log('TRANSCRIPTION', transcription);
+//   })
+//   .then(transcription => {
+//     //Set up tone analyzer params
+//     let params = {
+//       tone_input: { text: transcription },
+//       content_type: 'application/json'
+//     };
+//     // Do tone analysis
+//     //   toneAnalyzer.tone(params, function(error, response) {
+//     //     if (error) {
+//     //       console.log('error:', error);
+//     //     } else {
+//     //       // res.status(201).send({
+//     //       //   transcription: transcription,
+//     //       //   analysis: JSON.stringify(response)
+//     //       // });
+//     //     }
+//     // });
+//   })
+//   .catch(err => {
+//     console.error('ERROR:', err);
+//   });
